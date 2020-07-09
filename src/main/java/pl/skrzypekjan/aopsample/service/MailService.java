b7a3c08be9783c0457f736f12a7219701f9e2ee8
Jan Skrzypkowski <js.skrzypkowski@gmail.com>
2020-07-09 12:55:36 +0200

    Service resposible for sending e-mail

---
+++ src/main/java/pl/skrzypekjan/aopsample/service/MailService.java
@@ -0,0 +1,35 @@
+package pl.skrzypekjan.aopsample.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
+
+@Service
+public class MailService {
+
+    private JavaMailSender javaMailSender;
+
+    @Autowired
+    public MailService(JavaMailSender javaMailSender) {
+        this.javaMailSender = javaMailSender;
+    }
+
+    public void sendEmail(String to, String title, String content) {
+        MimeMessage mail = javaMailSender.createMimeMessage();
+        try {
+            MimeMessageHelper helper = new MimeMessageHelper(mail, true);
+            helper.setTo(to);
+            helper.setSubject(title);
+            helper.setText(content, true);
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+        javaMailSender.send(mail);
+    }
+}
