4f1fe5d8fc9b0e1f2e6e7560ebabb119af613f3c
Jan Skrzypkowski <js.skrzypkowski@gmail.com>
2020-07-09 13:00:17 +0200

    Specify access and setting

---
+++ src/test/java/pl/skrzypekjan/aopsample/AopSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.skrzypekjan.aopsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AopSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
